d5acb02b95c792f936b0d5c753c5f4fe1b055cae
rajanshah23 <shahrajan774@gmail.com>
2025-05-27 01:25:16 +0545

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
-import express from "express";
-import userRoute from "./routes/userRoute";
-import dotenv from "dotenv";
-import showRoute from "./routes/showRoute";
-import seatRoute from "./routes/seatRoute";
-import bookingRoutes from "./routes/bookingRoute";
-import reviewRoute from "./routes/reviewRoute";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoute from './routes/userRoute';
+import showRoute from './routes/showRoute';
+import seatRoute from './routes/seatRoute';
+import bookingRoutes from './routes/bookingRoute';
+import reviewRoute from './routes/reviewRoute';
+
 dotenv.config();
-require("./database/connection");
+require('./database/connection');
 
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "Rest API is working",
-  });
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'success', message: 'Rest API is working' });
 });
 
-app.use("/api/auth", userRoute);
-app.use("/api/shows", showRoute);
-app.use("/api/shows", seatRoute);
-app.use("/api/booking", bookingRoutes);
-app.use("/api/reviews", reviewRoute);
+app.use('/api/auth', userRoute);
+app.use('/api/shows', showRoute);
+app.use('/api/shows', seatRoute);
+app.use('/api/booking', bookingRoutes);
+app.use('/api/reviews', reviewRoute);
 
 export default app;
