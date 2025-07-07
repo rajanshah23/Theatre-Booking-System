522aa511c3704a68e80d8ed65cebabeb0ca5d7dd
rajanshah23 <shahrajan774@gmail.com>
2025-07-07 23:39:41 +0545

    final commit

--- src/sync.ts
+++ src/sync.ts
@@ -2,7 +2,7 @@ import { sequelize } from './database/connection';
 
 (async () => {
   try {
-    await sequelize.sync({force: true});
+    await sequelize.sync({force:false });
     console.log('All tables created successfully!');
   } catch (error) {
     console.error('Error syncing database:', error);
